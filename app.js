0fd7a78daf7282bb7147def85542358b380935b3
김민지 <qniomo00@gmail.com>
2025-02-20 10:48:43 +0900

    server 뼈대 작성

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import http from "http";
 import fs from "fs";
 import qs from "querystring";
+
+const server = http.createServer((req, res) => {});
+
+//8000번 포트 사용
+PORT = 8000;
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
