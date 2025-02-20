99f7f916eb950d9580deffb3f0da189e250f9a19
김민지 <qniomo00@gmail.com>
2025-02-20 11:21:44 +0900

    데이터 객체 형식 예시 작성

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ import qs from "querystring";
 //[ ]추가하기(/add)버튼
 //[ ]글 목록 (ul, li)
 
-//form태그로 받아올 데이터 객체 형식 정하기
+//[x]form태그로 받아올 데이터 객체 형식 정하기
+//{date : '2025-02-20',name : '하리보', content: '작은 사이즈 봉투에 들어있음'}
 
-//liTag 만드는 함수
+//[ ]liTag 만드는 함수
 
-//liTag를 사용해서 ul태그 만드는 함수
+//[ ]liTag를 사용해서 ul태그 만드는 함수
 
-//만들어진 ul태그를 사용해서 홈페이지 문자열 만드는 함수
+//[ ]만들어진 ul태그를 사용해서 홈페이지 문자열 만드는 함수
 
 const server = http.createServer((req, res) => {
   //req.method, req.url 확인
