2a9334e78cbc294324452a50c73ea36817bd1e18
muunji <twoji3019@naver.com>
2025-02-21 14:27:54 +0900

    페이지를 다시 돌아감 + alert 메시지 띄우기

--- app.js
+++ app.js
@@ -243,8 +243,7 @@ const server = http.createServer((req, res) => {
         let day = Number(dataObj.date.slice(-2))
         console.log (year,month,day)
         if(!(year===2025 && (month>0||month<13)&&(day>0||day<32))){
-          // res.write('다시 입력하세요');
-          window.alert('다시 입력하세요')
+          //[ ]addPage로 돌아감 + alert 안내 메시지
         }
         //[x]파일이 없으면 json파일을 만들고 빈 배열 넣어주기
         //indexHtml함수 안에서 처리 > 서버 실행되면 바로 진행됨
