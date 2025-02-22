c38dbac6891dfee253af31ec12289863abb99144
muunji <twoji3019@naver.com>
2025-02-22 09:41:53 +0900

    안보이는 input을 추가함

--- app.js
+++ app.js
@@ -118,6 +118,7 @@ function indexHtml() {
 // muunji/issue10-1-1에서 실험 결과 > input타입 변경, 속성 추가하기로 함
 //[x] 데이터 유효성 검사를 위한 script넣을 변수 만들어주기
 //[x] alert을 위한 함수 작성
+//[ ]form태그에서 랜덤 숫자 가져오기
 function alert(){
   return `<script>alert("'2025MMDD'의 형식을 지켜주세요")</script>`
 }
@@ -130,77 +131,78 @@ function addHtml(when) {
   }
   let htmlString = `
   <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>ADD</title>
-<style>
+  <html lang="en">
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ADD</title>
+    <style>
       body {
         margin: 0;
         padding: 0;
         width: 100%;
         height: 100vh;
-        }
-        #root {
-          width: 100%;
+      }
+      #root {
+        width: 100%;
         display: flex;
         flex-direction: column;
         align-items: center;
-        }
-        #root > section {
-          width: 90%;
-          }
-          #root > section > form {
-            width: 100%;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
+      }
+      #root > section {
+        width: 90%;
+      }
+      #root > section > form {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
         gap: 20px;
       }
       form > div:last-of-type {
         display: flex;
         gap: 10px;
-        }
+      }
       form > div:last-of-type textarea {
         width: 380px;
-        }
-        form > button {
-          border-style: none;
-          padding: 5px 10px;
-          cursor: pointer;
-          background-color: #ccc;
-          }
-          form > button:hover {
-            background-color: black;
-            color: #fff;
       }
-      </style>
-      </head>
-      <body>
+      form > button {
+        border-style: none;
+        padding: 5px 10px;
+        cursor: pointer;
+        background-color: #ccc;
+      }
+      form > button:hover {
+        background-color: black;
+        color: #fff;
+      }
+    </style>
+  </head>
+  <body>
     <div id="root">
-    <h1>간식 추가</h1>
-    <section>
-    <form action="/plus" method="POST">
-    <div>
-    <label for="date">간식 먹은 날</label>
-    <input type="number" name="date" placeholder="2025MMDD" required/>
-    <label for="name">간식</label>
+      <h1>간식 추가</h1>
+      <section>
+        <form action="/plus" method="POST">
+          <input type="hidden" name="id">
+          <div>
+            <label for="date">간식 먹은 날</label>
+            <input type="number" name="date" placeholder="2025MMDD" required/>
+            <label for="name">간식</label>
             <input type="text" name="name" placeholder="간식 이름" required/>
-            </div>
-            <div>
+          </div>
+          <div>
             <label for="content">내용</label>
             <textarea
             name="content"
               placeholder="후기나 하고싶은 말"
             ></textarea>
-            </div>
-            <button type="submit">작성완료</button>
-            </form>
-            </section>
-            </div>
-            ${when}
-            </body>
+          </div>
+          <button type="submit">작성완료</button>
+        </form>
+      </section>
+    </div>
+    ${when}
+  </body>
 </html>
 `;
   return htmlString;
