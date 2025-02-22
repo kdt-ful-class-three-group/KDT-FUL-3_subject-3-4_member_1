b3b9e2b218a92d3a4879cf3b5a68f0d44639ca6e
muunji <twoji3019@naver.com>
2025-02-22 13:03:56 +0900

    함수 로직에 의해 문자열을 붙여줌 -> 성공

--- app.js
+++ app.js
@@ -550,9 +550,8 @@ const server = http.createServer((req, res) => {
         fs.writeFileSync('list.json',JSON.stringify(list))
         //다시 상세페이지로 돌아가야함
         //date가 바뀔 수 있음 -> url에 /edit/이 지워지고 id와 name 정보가 들어가야함
-        let url = data.split('&',2).join('&')
         res.writeHead(200,{'content-type':'text.html; charset=utf-8'})
-        res.write(detailHtml(url));
+        res.write(detailHtml('/'+data));
         res.end()
       })
     }
