74501a5b612a9e8d25dfd81812c48e4d683a1d48
muunji <twoji3019@naver.com>
2025-02-23 17:21:11 +0900

    read와 관련된 함수 가져옴

--- method/read.js
+++ method/read.js
@@ -1,7 +1,30 @@
 import fs from 'fs'
 
 const read = {
-  readFunc:function(){
-    
+  //server 안에서 받복되는 내용 함수로 작성
+  readFunc:function(res,callback){
+    //content-type은 항상 html
+    res.writeHead(200,{'content-type':'text/html; charset=utf-8'})
+    //경로에 따라 함수가 바뀜
+    res.write(callback);
+    res.end()
+  },
+  //404에러 함수
+  readErr : function(res){
+    res.writeHead(404,{'content-type':'text/plain; charset=utf-8'})
+    res.write('NOT FOUNT')
+    res.end()
+  },
+  //리다이렉트
+  readLocation : function(res,callback){
+    res.writeHead(302,{location:'/admin'})
+    res.write(callback)
+    res.end()
+  },
+  //list.json 데이터를 객체로 변경
+  readList : function(){
+    let listJson = fs.readFileSync('list.json')
+    let list = JSON.parse(listJson)
+    return list
   }
 }
