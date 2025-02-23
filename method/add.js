4806977b88e1b6cc9f8a4dbd22846f7a7d5140b1
muunji <twoji3019@naver.com>
2025-02-23 15:45:56 +0900

    id에 적용할 함수, 데이터 유효성 검사에 사용할 함수 가져옴

--- method/add.js
+++ method/add.js
@@ -1,1 +1,11 @@
-const add = {}
+const add = {
+  randomId: function() {
+    return `
+    let id = document.getElementsByTagName('input')[0]
+    id.value = Math.floor(Math.random()*899999)+100000;
+  `
+  },
+  alert: function(){
+    return `alert("'2025MMDD'의 형식을 지켜주세요")`
+  }
+}
