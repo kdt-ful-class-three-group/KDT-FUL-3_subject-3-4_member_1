0c4c50c1cb7a95d59d3ebadb7a15df2903c854a1
muunji <twoji3019@naver.com>
2025-02-23 16:04:05 +0900

    수정, 삭제 태그 생성 함수

---
+++ method/detail.js
@@ -0,0 +1,11 @@
+const detail = {
+  btnTag : function(url){
+    return `<section>
+      <ul>
+        <li><a href="/edit/${url}">수정</a></li>
+        <li><a href="/delete/${url}">삭제</a></li>
+      </ul>
+    </section>`
+  },
+  
+}
