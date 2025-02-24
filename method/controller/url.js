d53b9379f1384367f13456bec4c1fd881f8bfea2
김민지 <qniomo00@gmail.com>
2025-02-24 16:18:28 +0900

    home.js에서 adminUrl 분리

--- method/controller/url.js
+++ method/controller/url.js
@@ -8,7 +8,11 @@ const url = {
     //&로 나눈 배열에서 앞순서 2개 가져온 후 요소 사이에 &붙여 문자열로 반환
     return dataString.split('&',2).join('&')
   },
+  /**
+   * admin로 접근했을 때 url에 적용할 함수
+   * @return {string} admin/id=111111&date='250101' 
+   */
   adminUrl : function(dataString){
-
+    return `admin/${url.makeUrl(dataString)}`
   }
 }
