f9fce4c8957ce34c351a9cb6186edf28a28ceda3
김민지 <qniomo00@gmail.com>
2025-02-24 15:52:19 +0900

    home.js에서 home.url 분리

---
+++ method/controller/url.js
@@ -0,0 +1,14 @@
+//url에선 id와 date만 사용함
+const url = {
+  /**
+   * 문자열로 바뀐 객체에서 id와 date를 가져오는 함수
+   * @return {string} id=111111&date='20250101'
+   */
+  makeUrl : function(dataString){
+    //&로 나눈 배열에서 앞순서 2개 가져온 후 요소 사이에 &붙여 문자열로 반환
+    return dataString.split('&',2).join('&')
+  },
+  adminUrl : function(dataString){
+
+  }
+}
