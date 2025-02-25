c831341a55513993b3fb757ec265e310eb4070c0
김민지 <qniomo00@gmail.com>
2025-02-25 11:24:35 +0900

    json파일 불러옴

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ server.listen(8000,()=>{
 
 //json파일에 대한 서버
 const jsonServer =http.createServer((req,res)=>{
-
+  res.writeHead(200,{'content-type':'application/json; charset=utf-8'})
+  res.write(fs.readFileSync('list.json'))
+  res.end()
 })
 
 jsonServer.listen(8010,()=>{
