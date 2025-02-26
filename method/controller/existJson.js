dad40d2b42106dbd2feccd7288bbb322338d169c
김민지 <qniomo00@gmail.com>
2025-02-26 15:40:27 +0900

    existJson.js 내보내기

--- method/controller/existJson.js
+++ method/controller/existJson.js
@@ -1,4 +1,15 @@
+import exp from 'constants';
+import fs from 'fs'
 
 const existJson = {
-
+  /**
+   * list.json 파일이 존재하지 않을 때 빈 배열을 JSON문자열로 갖는 파일 생성
+   */
+  check : function(){
+    if (!fs.existsSync("list.json")) {
+      fs.writeFileSync("list.json", JSON.stringify([]));
+    }
+  }
 }
+
+export default existJson
