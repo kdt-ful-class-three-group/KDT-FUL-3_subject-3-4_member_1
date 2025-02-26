0f05578693651c052dc7da9d340cd299a48d6509
김민지 <qniomo00@gmail.com>
2025-02-26 16:00:46 +0900

    html문자열 함수로 작성

--- method/view/html.js
+++ method/view/html.js
@@ -4,20 +4,78 @@ import read from '../read.js'
 import exist from '../controller/existJson.js'
 //tag모듈
 import tag from './tag.js'
-//makeurl모듈
-import url from '../controller/url.js'
 
 const html = {
-  home : function(){
+  /**
+   * '/' 경로에 따른 홈페이지
+   * @param {Boolean} isAdmin - admin이면 true, 아니면 false 
+   * @param {Function} callback - url을 결정할 함수 
+   * @returns {String} 홈페이지 문자열
+   */
+  home : function(isAdmin,callback){
+
+    //isAdmin에 따라 aTag유무 결정
+    let addATag = isAdmin?tag.hrefATag('/add','추가'):''
 
     //json파일 존재하는지 확인 후 없으면 생성
     exist.check()
-    //json파일 있으면 가져옴
-    let list = read.readList()
-
-    let ul = tag.ulTag(url.makeUrl,list)
-    console.log(ul)
-
+    
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>HOME</title>
+          <style>
+            body {
+              margin: 0;
+              padding: 0;
+              width: 100%;
+              height: 100vh;
+            }
+            a {
+              text-decoration: none;
+              color: black;
+            }
+            ul {
+              list-style: none;
+            }
+            #root {
+              width: 100%;
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+            }
+            #root > section {
+              width: 90%;
+            }
+            section > a {
+              background-color: #ccc;
+              padding: 5px 10px;
+            }
+            section > a:hover {
+              background-color: black;
+              color:white;
+            }
+            ul > li {
+              margin-bottom: 10px;
+            }
+            ul > li > a:hover {
+              color: #ccc;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="root">
+            <h1>오늘 간식</h1>
+            <section>
+              ${addATag}
+              ${tag.ulTag(callback,read.readList())}
+            </section>
+          </div>
+        </body>
+      </html>`
   }
 }
 
