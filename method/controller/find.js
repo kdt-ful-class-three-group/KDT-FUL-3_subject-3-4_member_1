97f34e4e1d726834f5968539762e5a212a2f64da
김민지 <qniomo00@gmail.com>
2025-02-26 16:40:39 +0900

    JSDoc 작성

--- method/controller/find.js
+++ method/controller/find.js
@@ -5,14 +5,19 @@ import urlSlice from '../../data/urlSlice.js'
 
 const find = {
   /**
-   * 
+   * url을 객체로 변환하는 함수
    * @param {String} url - id와 name이 있는 req.url
    * @param {String} urlRole - 'url' or 'edit' or 'admit' or 'delete'
-   * @returns {Object} - id와 date를 가진 객체
+   * @returns {Object} id와 date를 가진 객체
    */
   urlToObj : function(url,urlRole){
     return qs.parse(url.slice(urlSlice[urlRole]))
   },
+  /**
+   * url의 id와 date가 list.json에 있는지 확인하는 함수
+   * @param {Array} arr - 배열로 된 데이터 
+   * @returns {Object} id와 date가 일치하는 배열의 요소
+   */
   filter : function(arr){
     return arr.filter(i=> (i.id === find.urlToObj.id)&&(i.date===find.urlToObj.date))[0]
   }
