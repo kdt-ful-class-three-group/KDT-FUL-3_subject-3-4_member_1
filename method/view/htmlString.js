f20463446a219e60c7132f9841d173b02e905601
muunji <twoji3019@naver.com>
2025-03-02 01:33:29 +0900

    home htmlString 분리

--- method/view/htmlString.js
+++ method/view/htmlString.js
@@ -1,5 +1,61 @@
 const htmlString = {
-  homeHtml : function(){
-    
+  homeHtml : function(addATag,callback){
+    return `
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>HOME</title>
+          <style>
+            body {
+              margin: 0;
+              padding: 0;
+              width: 100%;
+              height: 100vh;
+            }
+            a {
+              text-decoration: none;
+              color: black;
+            }
+            ul {
+              list-style: none;
+            }
+            #root {
+              width: 100%;
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+            }
+            #root > section {
+              width: 90%;
+            }
+            section > a {
+              background-color: #ccc;
+              padding: 5px 10px;
+            }
+            section > a:hover {
+              background-color: black;
+              color:white;
+            }
+            ul > li {
+              margin-bottom: 10px;
+            }
+            ul > li > a:hover {
+              color: #ccc;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="root">
+            <h1>오늘 간식</h1>
+            <section>
+              ${addATag}
+              ${callback}
+            </section>
+          </div>
+        </body>
+      </html>
+    `
   }
 }
