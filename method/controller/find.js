90d9d3059f17fdf7629b401e2234d161d4b385e2
muunji <twoji3019@naver.com>
2025-03-02 18:52:45 +0900

    detail 페이지에 사용할 로직 함수 작성

--- method/controller/find.js
+++ method/controller/find.js
@@ -18,19 +18,34 @@ const find = {
   /**
    * url의 id와 date가 list.json에 있는지 확인하는 함수
    * @param {Array} arr - 배열로 된 데이터 
+   * @param {String} url - id와 name이 있는 req.url
+   * @param {String} urlRole -'url' or 'edit' or 'admit' or 'delete'
    * @returns {Object} id와 date가 일치하는 배열의 요소
    */
   filter : function(arr,url,urlRole){
     return arr.filter(i=> (i.id === find.urlToObj(url,urlRole).id)&&(i.date===find.urlToObj(url,urlRole).date))[0]
   },
-  detailFilter : function(url,isAdmin){
+  /**
+   * 
+   * @param {Array} arr - 배열로 된 데이터 
+   * @param {String} url - id와 name이 있는 req.url 
+   * @param {boolean} isAdmin - admin경로인지 true, false 
+   * @returns {object} html문자열에 들어갈 데이터가 답긴 data, btn문자열의 유무가 담긴 isBtn을 키로 가진 객체
+   */
+  detailFilter : function(arr,url,isAdmin){
     let obj = {}
     if(isAdmin){
-      obj.urlObj = find.urlToObj(url,urlSlice.admin)
-      obj.isBtn = tag.btnSectionTag(url)
+      obj = {
+        data : find.filter(arr,url,'admin'),
+        btn : tag.btnSectionTag(url),
+        url : 'admin'
+      }
     } else {
-      obj.urlObj = find.urlToObj(url,urlSlice.url)
-      obj.isBtn = ''
+      obj = {
+        data:find.filter(arr,url,'url'),
+        btn : '',
+        url : ''
+      }
     }
     return obj
   }
