f7428984b75298c72753a5da119988d014b26fcd
muunji <twoji3019@naver.com>
2025-03-02 19:48:24 +0900

    get /

--- start.js
+++ start.js
@@ -5,6 +5,14 @@ import qs from 'querystring'
 const server = http.createServer((req,res)=>{
   //method url 확인
   console.log(`${req.method} ${req.url}`)
+
+  //GET
+  if(req.method === 'GET'){
+    //진입
+    if(req.url==='/'){
+      
+    }
+  }
 })
 
 server.listen(3080,()=>{
